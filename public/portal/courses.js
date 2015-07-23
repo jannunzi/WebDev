f33e1103df27470449982765fd5691d27d4161d6
maddy2308 <madhur.eng@gmail.com>
2015-07-23 00:12:19 -0700

    Fixed the Module 1, with overview pill fixed.

--- public/portal/courses.js
+++ public/portal/courses.js
@@ -1,54 +1,129 @@
 ﻿(function () {
-    angular.courses = [
+  angular.courses = [
     {
-        title: 'CS5610',
-        modules: [
+      title: 'CS5610',
+      modules: [
+        {
+          title: 'Module 0',
+          lectures: [
             {
-                title: 'Modulo AAA Asd',
-                lectures: [
-                    {
-                        title: 'Lectura 0'
-                    },
-                    {
-                        title: 'Lectura 1'
-                    },
-                    {
-                        title: 'Lectura 2'
-                    }
-                ],
-                assignments: [
-                    {
-                        title: 'Tarea 0'
-                    },
-                    {
-                        title: 'Tarea 1'
-                    }
-                ],
-                examples: [
-                    {
-                        title: 'Ejemplo 0'
-                    }
-                ],
-                videos: [],
-                slides: []
+              title: 'Lecture 0'
             },
             {
-                title: 'Modulo 1',
-                lectures: [],
-                assignments: [],
-                videos: [],
-                slides: [],
-                examples: []
+              title: 'Lecture 1'
             },
             {
-                title: 'Module 2',
-                lectures: [],
-                assignments: [],
-                videos: [],
-                slides: [],
-                examples: []
+              title: 'Lecture 2'
             }
-        ]
+          ],
+          assignments: [
+            {
+              title: 'Assignment 0'
+            },
+            {
+              title: 'Assignment 1'
+            }
+          ],
+          examples: [
+            {
+              title: 'Example 0'
+            },
+            {
+              title: 'Example 1'
+            }
+          ],
+          experiments: [
+            {
+              title: 'Experiment 0'
+            },
+            {
+              title: 'Experiment 1'
+            }
+          ],
+          videos: [],
+          slides: []
+        },
+        {
+          title: 'Module 1',
+          lectures: [
+            {
+              title: 'Lecture 0'
+            },
+            {
+              title: 'Lecture 1'
+            },
+            {
+              title: 'Lecture 2'
+            }
+          ],
+          assignments: [
+            {
+              title: 'Assignment 0'
+            },
+            {
+              title: 'Assignment 1'
+            }
+          ],
+          examples: [
+            {
+              title: 'Example 0'
+            },
+            {
+              title: 'Example 1'
+            }
+          ],
+          experiments: [
+            {
+              title: 'Experiment 0'
+            },
+            {
+              title: 'Experiment 1'
+            }
+          ],
+          videos: [],
+          slides: []
+        },
+        {
+          title: 'Module 2',
+          lectures: [
+            {
+              title: 'Lecture 0'
+            },
+            {
+              title: 'Lecture 1'
+            },
+            {
+              title: 'Lecture 2'
+            }
+          ],
+          assignments: [
+            {
+              title: 'Assignment 0'
+            },
+            {
+              title: 'Assignment 1'
+            }
+          ],
+          examples: [
+            {
+              title: 'Example 0'
+            },
+            {
+              title: 'Example 1'
+            }
+          ],
+          experiments: [
+            {
+              title: 'Experiment 0'
+            },
+            {
+              title: 'Experiment 1'
+            }
+          ],
+          videos: [],
+          slides: []
+        }
+      ]
     }
-    ];
+  ];
 })();
--- public/portal/navController.js
+++ public/portal/navController.js
@@ -7,28 +7,40 @@
     function NavController($location)
     {
       var vm = this;
-      console.log("Nav");
       vm.course = angular.courses[0];
       vm.selectModule = selectModule;
       vm.selectTab = selectTab;
       vm.selectPillIndex = selectPillIndex;
+      vm.selectedModuleIndex = 0;
       
       function selectPillIndex(pillIndex)
       {
-        console.log(pillIndex);
-//        $location.url("modules/")
         var url = $location.url();
         url = url.slice(0, url.lastIndexOf('/')+1) + pillIndex;
-        console.log(url);
         $location.url(url);
       }
       
       function selectTab(tabName)
       {
-        console.log(tabName);
-        console.log(vm.selectedModuleIndex);
-        vm.pills = vm.course.modules[vm.selectedModuleIndex][tabName];
-        $location.url("modules/"+vm.selectedModuleIndex+"/"+tabName+"/0");
+        if (!(tabName.toLowerCase() === "Overview".toLowerCase())) {
+          vm.pills = vm.course.modules[vm.selectedModuleIndex][tabName];
+          $location.url("modules/" + vm.selectedModuleIndex + "/" + tabName + "/0");
+        } else {
+          // if overview is clicked
+          var url = $location.url();
+          if (url.search("module") == -1) {
+            // case 1: when no module is selected, default to module 1
+            url = url + "#/modules/0";
+            $location.url(url);
+          } else {
+            // case 2: when module keyword is in url, then fetch the module number and set that as the default modul
+            var startPosn = url.search("module");
+            var slicedUrl = url.slice(startPosn + "module".length + 2);
+            var loadedModule = slicedUrl.slice(0,slicedUrl.indexOf("/"));
+            url = "modules/" + loadedModule;
+            $location.url(url);
+          }
+        }
       }
       
       function selectModule(index)
