64dde58ba819ade180364109fec4911237ac65bb
maddy2308 <madhur.eng@gmail.com>
2015-08-09 21:56:00 -0700

    Bug fixing for the uri to show apt module in UI and in browser URL also the same for the pill(sub section withing the tabs)

--- public/portal/navController.js
+++ public/portal/navController.js
@@ -4,17 +4,27 @@
       .module('app')
       .controller("NavController", NavController);
 
-    function NavController($location)
-    {
+    function NavController($location){
       var vm = this;
-
-      // variable declaration on scope
       vm.course = angular.courses[0];
-      vm.pills = [];
-      vm.selectedModuleIndex = 1;
-      vm.tabSelected = 'overview';
-      vm.pillSelected = '';
-      vm.moduleSelected = vm.course.modules[1].title;
+
+      var url = $location.url();
+      if(url.search("modules/") == -1) {
+        // variable declaration on scope
+        vm.pills = [];
+        vm.selectedModuleIndex = 1;
+        vm.tabSelected = 'overview';
+        vm.pillSelected = '';
+        vm.moduleSelected = vm.course.modules[1].title;
+      } else {
+        var moduleNumber = setModuleNumber(url);
+        url = "modules/" + moduleNumber[0];
+        $location.url(url);
+        vm.selectedModuleIndex = moduleNumber[0];
+        vm.tabSelected = 'overview';
+        vm.pillSelected = '';
+        vm.moduleSelected = vm.course.modules[vm.selectedModuleIndex].title;
+      }
 
       // method declaration on scope
       vm.selectModule = selectModule;
@@ -35,6 +45,7 @@
         vm.tabSelected = tabName;
         if (!(tabName.toLowerCase() === "Overview".toLowerCase())) {
           vm.pills = vm.course.modules[vm.selectedModuleIndex][tabName];
+          vm.pillSelected = vm.pills[0].title;
           $location.url("modules/" + vm.selectedModuleIndex + "/" + tabName + "/0");
         } else {
           // if overview is clicked
@@ -45,20 +56,28 @@
             $location.url(url);
           } else {
             // case 2: when module keyword is in url, then fetch the module number and set that as the default module
-            var regEx = new RegExp('modules/\\d+');
-            var matches = url.match(regEx);
-            if(matches && matches.length == 1) {
-              var moduleNumber = matches[0].match('\\d+');
-              url = "modules/" + moduleNumber[0];
-              $location.url(url);
-            }
+            var moduleNumber = setModuleNumber(url);
+            url = "modules/" + moduleNumber[0];
+            $location.url(url);
           }
+        }
+      }
+
+      function setModuleNumber(url) {
+        var regEx = new RegExp('modules/\\d+');
+        var matches = url.match(regEx);
+        if(matches && matches.length == 1) {
+          var moduleNumber = matches[0].match('\\d+');
+          return moduleNumber;
         }
       }
       
       function selectModule(index){
+        vm.pills = [];
         vm.moduleSelected = vm.course.modules[index].title;
         vm.selectedModuleIndex = index;
+        vm.tabSelected = 'overview';
+        vm.pillSelected = '';
       }
     }
 })();
