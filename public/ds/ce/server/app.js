e5a65b7afc0eb59c6dce005f02bc50f1b21bd5b6
Maria Irizarry <mirizarry@Marias-MacBook-Pro.local>
2015-11-18 23:44:44 -0800

    created mongoose schema for courses

--- public/ds/ce/server/app.js
+++ public/ds/ce/server/app.js
@@ -1,3 +1,60 @@
 module.exports = function(app, db, mongoose) {
     require("./services/course.service.js")(app, db, mongoose);
+
+    var Schema = mongoose.Schema;
+
+    var coursesSchema = new Schema({
+        courses: [
+            {
+                title: String,
+                modules: [{
+                    title: String,
+                    available: Boolean,
+                    visible: Boolean,
+                    lectures: [
+                        {
+                            title: String
+                        }
+                    ],
+                    assignments: [
+                        {
+                            title: String
+                        }
+                    ],
+                    videos: [
+                        {
+                            title: String,
+                            src: String
+                        }
+                    ],
+                    slides: [
+                        {
+                            title: String,
+                            src: String
+                        }
+                    ],
+                    examples: [
+                        {
+                            title: String,
+                            ex: [
+                                {
+                                    title: String,
+                                    base: String,
+                                    src: String,
+                                    dependencies: [
+                                        {
+                                            title: String,
+                                            src: String
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+
+
+                }]
+            }]
+    });
+
 };
