78eb4f4b8bd2840be77704ff08c0dd0a4feb0886
jannunzi <jannunzi@gmail.com>
2016-01-05 09:44:36 -0500

    Braintree

--- public/experiments/braintree/server/app.js
+++ public/experiments/braintree/server/app.js
@@ -1,11 +1,37 @@
+var braintree = require("braintree");
+
+var gateway = braintree.connect({
+    environment: braintree.Environment.Sandbox,
+    merchantId: "tcjtq7fcjbttnfb5",
+    publicKey: "3yfhgcysvp6dzxw8",
+    privateKey: "c4c214a0904450885fae523afdfb47a9"
+});
+
 module.exports = function(app){
 
     console.log("ewq");
 
     app.post("/checkout", checkout);
+    app.get("/client_token", getClientToken);
+
+    function getClientToken (req, res) {
+        gateway.clientToken.generate({}, function (err, response) {
+            res.send(response.clientToken);
+        });
+    }
 
     function checkout(req, res) {
-        var body = req.body;
-        res.json(body);
+        var nonceFromTheClient = req.body.payment_method_nonce;
+
+        gateway.transaction.sale({
+            amount: '10.00',
+            paymentMethodNonce: nonceFromTheClient,
+        }, function (err, result) {
+
+        });
+
+        //var body = req.body;
+        //res.json(body);
+        res.send(req.body);
     }
 }
