f18926e92e0a45cc6d738c41a9940b11cedce64c
JoseAnnunziatoNeu <jga@ccs.neu.edu>
2016-02-29 14:57:51 -0500

    Added home page

--- public/project/omdb/client/app.js
+++ public/project/omdb/client/app.js
@@ -1,4 +1,4 @@
 (function(){
     angular
-        .module("OmdbApp", []);
+        .module("OmdbApp", ["ngRoute"]);
 })();
---
+++ public/project/omdb/client/config.js
@@ -0,0 +1,15 @@
+(function(){
+    angular
+        .module("OmdbApp")
+        .config(configuration);
+
+    function configuration($routeProvider) {
+        $routeProvider
+            .when("/home", {
+                templateUrl: "views/home/home.view.html"
+            })
+            .otherwise({
+                redirectTo: "/home"
+            });
+    }
+})();
