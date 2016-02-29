e42bdf7fa23c2fc6ac9ac1803e9ed8fe01650cd9
JoseAnnunziatoNeu <jga@ccs.neu.edu>
2016-02-29 15:34:35 -0500

    Added navigation controller

---
+++ public/project/omdb/client/views/header/navigation.controller.js
@@ -0,0 +1,14 @@
+(function(){
+    angular
+        .module("OmdbApp")
+        .controller("NavigationController", navigationController);
+
+    function navigationController() {
+        var vm = this;
+
+        function init() {
+
+        }
+        init();
+    }
+})();
