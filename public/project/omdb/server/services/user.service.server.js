fd3a1ffa13ac57d62c7b9a6f11c5f281d7265254
JoseAnnunziatoNeu <jga@ccs.neu.edu>
2016-02-29 18:04:51 -0500

    store current in session added loggedin user server service

--- public/project/omdb/server/services/user.service.server.js
+++ public/project/omdb/server/services/user.service.server.js
@@ -1,9 +1,15 @@
 module.exports = function(app, model) {
     app.post("/api/project/login", findUserByCredentials);
+    app.get("/api/project/loggedin", loggedin);
 
     function findUserByCredentials(req, res) {
         var credentials = req.body;
         var user = model.findUserByCredentials(credentials);
+        req.session.currentUser = user;
         res.json(user);
+    }
+
+    function loggedin(req, res) {
+        res.json(req.session.currentUser);
     }
 }
