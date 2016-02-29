f9b60e64b2cde76c45a79cdd44feecddc7547cd4
JoseAnnunziatoNeu <jga@ccs.neu.edu>
2016-02-29 16:40:32 -0500

    added logout link only show logout if logged in added profile link show profile link only if logged in hide profile link if logged in

--- public/project/omdb/client/views/header/navigation.controller.js
+++ public/project/omdb/client/views/header/navigation.controller.js
@@ -3,12 +3,19 @@
         .module("OmdbApp")
         .controller("NavigationController", navigationController);
 
-    function navigationController($location) {
+    function navigationController($location, UserService) {
         var vm = this;
+
+        vm.logout = logout;
 
         function init() {
             vm.$location = $location;
         }
         init();
+
+        function logout() {
+            UserService.setCurrentUser(null);
+            $location.url("/home");
+        }
     }
 })();
