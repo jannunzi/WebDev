6b4584ec24ba48187a5aa36202c4a4df47cc284c
JoseAnnunziatoNeu <jga@ccs.neu.edu>
2016-02-29 23:51:59 -0500

    added create movie to movie server model

--- public/project/omdb/server/models/movie.model.server.js
+++ public/project/omdb/server/models/movie.model.server.js
@@ -1,9 +1,19 @@
 module.exports = function() {
     var movies = [];
     var api = {
-        findMovieByImdbID: findMovieByImdbID
+        findMovieByImdbID: findMovieByImdbID,
+        createMovie: createMovie
     };
     return api;
+
+    function createMovie(imdbID) {
+        var movie = {
+            _id: "ID_" + (new Date()).getTime(),
+            imdbID: imdbID
+        };
+        movies.push(movie);
+        return movie;
+    }
 
     function findMovieByImdbID(userId, imdbID) {
         for(var m in movies) {
