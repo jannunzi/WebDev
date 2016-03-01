6f1489e2f0e22d497e0305a536d5f322e97138f1
JoseAnnunziatoNeu <jga@ccs.neu.edu>
2016-02-29 21:47:14 -0500

    get imdbID from route params

--- public/project/omdb/client/views/details/details.controller.js
+++ public/project/omdb/client/views/details/details.controller.js
@@ -3,8 +3,10 @@
         .module("OmdbApp")
         .controller("DetailsController", detailsController);
 
-    function detailsController() {
+    function detailsController($routeParams) {
         var vm = this;
+        var imdbID = $routeParams.imdbID;
+        console.log(imdbID);
 
         function init() {
             console.log("Details Controller");
