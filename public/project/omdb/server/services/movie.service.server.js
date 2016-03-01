74c9bda7f9e0a79a22fb9e6f51d8992630f55503
JoseAnnunziatoNeu <jga@ccs.neu.edu>
2016-02-29 23:52:09 -0500

    added create movie to movie server model

--- public/project/omdb/server/services/movie.service.server.js
+++ public/project/omdb/server/services/movie.service.server.js
@@ -5,12 +5,13 @@ module.exports = function(app, movieModel) {
         var userId = req.params.userId;
         var imdbID = req.params.imdbID;
         var movie = movieModel.findMovieByImdbID(imdbID);
-        if(movie) {
-            if(!movie.likes) {
-                movie.likes = [];
-            }
-            movie.likes.push(userId);
+        if(!movie) {
+            movie = movieModel.createMovie(imdbID);
         }
+        if(!movie.likes) {
+            movie.likes = [];
+        }
+        movie.likes.push(userId);
         console.log([userId, imdbID, movie]);
         res.send(200);
     }
