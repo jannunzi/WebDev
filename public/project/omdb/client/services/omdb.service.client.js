f5e9445c87fe5ea4a7299d5c557123136cda1b33
JoseAnnunziatoNeu <jga@ccs.neu.edu>
2016-03-13 22:30:01 -0400

    OMDB API JSONP

--- public/project/omdb/client/services/omdb.service.client.js
+++ public/project/omdb/client/services/omdb.service.client.js
@@ -11,11 +11,13 @@
         return api;
 
         function findMovieByImdbID(imdbID) {
-            return $http.get("http://www.omdbapi.com/?i="+imdbID);
+            // use JSONP since API does not support CORS
+            return $http.jsonp("http://www.omdbapi.com/?i="+imdbID+"&callback=JSON_CALLBACK");
         }
 
         function searchMovieByTitle(title) {
-            return $http.get("http://www.omdbapi.com/?s="+title);
+            // use JSONP since API does not support CORS
+            return $http.jsonp("http://www.omdbapi.com/?s="+title+"&callback=JSON_CALLBACK");
         }
     }
 })();
