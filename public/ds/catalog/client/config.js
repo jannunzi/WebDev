cbef1a02ec05354ca2439740e223b1faef8c0004
Ameya Pandilwar <ameya@pandilwar.com>
2016-03-20 20:25:22 -0400

    Configured app.js & server.js

---
+++ public/ds/catalog/client/config.js
@@ -0,0 +1,45 @@
+/**
+ * Created by ameyapandilwar on 3/18/16.
+ */
+
+(function () {
+    "use strict";
+    angular
+        .module("CatalogApp")
+        .config(configuration);
+
+    function configuration($routeProvider) {
+        $routeProvider
+            .when("/home", {
+                templateUrl: "views/home/home.view.html",
+                controller: "HomeController"
+            })
+            .when("/admin", {
+                templateUrl: "views/admin/admin.view.html",
+                controller: "AdminController"
+            })
+            .when("/login", {
+                templateUrl: "views/users/login.view.html",
+                controller: "LoginController"
+            })
+            .when("/register", {
+                templateUrl: "views/users/register.view.html",
+                controller: "RegisterController"
+            })
+            .when("/profile", {
+                templateUrl: "views/users/profile.view.html",
+                controller: "ProfileController"
+            })
+            .when("/schedule", {
+                templateUrl: "views/courses/schedule.view.html",
+                controller: "ScheduleController"
+            })
+            .when("/course", {
+                templateUrl: "views/courses/course.view.html",
+                controller: "CourseController"
+            })
+            .otherwise({
+                redirectTo: "/home"
+            });
+    }
+}());
