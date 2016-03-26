e1fcfd1d7109db7fa7d2390c9d20a9115782a22c
JoseAnnunziatoNeu <jga@ccs.neu.edu>
2016-03-26 09:50:39 -0400

    Retrieving uploaded file properties

--- experiments/upload/app.js
+++ experiments/upload/app.js
@@ -1,5 +1,18 @@
 module.exports = function(app) {
     app.post('/upload', function (req, res) {
 
+        // get file object from request
+        var myFile = req.files.myFile;
+
+        // path where file was written
+        var path = myFile.path;
+
+        // original file name
+        var name = myFile.name;
+
+        var size = myFile.size;
+        var type = myFile.type;
+
+        res.send(200);
     });
 }
