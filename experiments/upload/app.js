a5ffa3aa763d05b5b9bec7b117f461bde8a93903
JoseAnnunziatoNeu <jga@ccs.neu.edu>
2016-03-26 09:57:47 -0400

    Store file object in some array or collection

--- experiments/upload/app.js
+++ experiments/upload/app.js
@@ -1,17 +1,22 @@
 module.exports = function(app) {
+
+    var files = [];
+
     app.post('/upload', function (req, res) {
 
-        // get file object from request
         var myFile = req.files.myFile;
 
-        // path where file was written
-        var path = myFile.path;
+        // create local file object with relevant properties
+        var file = {
+            path: myFile.path,
+            name: myFile.name,
+            size: myFile.size,
+            type: myFile.type
+        }
 
-        // original file name
-        var name = myFile.name;
-
-        var size = myFile.size;
-        var type = myFile.type;
+        // store file object in some collection
+        // or store in a database collection
+        files.push(file);
 
         res.send(200);
     });
