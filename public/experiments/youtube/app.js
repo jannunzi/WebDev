59df0fd7d1d36d3dfaa2565f984117910454568b
JoseAnnunziatoNeu <jga@ccs.neu.edu>
2016-03-26 15:00:54 -0400

    Use AngularJS $sce (Strict Contextual Escaping) to escape YouTube URLs as safe

--- public/experiments/youtube/app.js
+++ public/experiments/youtube/app.js
@@ -5,9 +5,9 @@
 
     function YouTubeListController($scope, $sce) {
         var videos = [
-            {youTubeId : "weAe2NX5Vv4"},
-            {youTubeId : "qpKvDw0JIlo"},
-            {youTubeId : "LzpgJaoDW28"}
+            {youTubeId : "weAe2NX5Vv4", url: $sce.trustAsResourceUrl("http://www.youtube.com/embed/weAe2NX5Vv4")},
+            {youTubeId : "qpKvDw0JIlo", url: $sce.trustAsResourceUrl("http://www.youtube.com/embed/qpKvDw0JIlo")},
+            {youTubeId : "LzpgJaoDW28", url: $sce.trustAsResourceUrl("http://www.youtube.com/embed/LzpgJaoDW28")}
         ];
         $scope.videos = videos;
     }
