000509ef3ca13fef86a35d15105a0fb042c2c3ea
JoseAnnunziatoNeu <jga@ccs.neu.edu>
2016-03-27 21:52:49 -0400

    Document Passport JS

--- public/portal/app.js
+++ public/portal/app.js
@@ -1,6 +1,6 @@
 (function(){
   angular
-    .module("WhiteBoardApp", ['ngRoute'])
+    .module("WhiteBoardApp", ['ngRoute', 'gist'])
     .filter('underscore2space', function () {
       return function (value) {
           return (!value) ? '' : value.replace(/_/g, ' ').replace(/examples/, '');
