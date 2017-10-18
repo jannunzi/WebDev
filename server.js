e4f6b3ddb04a1f43db4abf183332181ca7a21b4f
jannunzi <jannunzi@gmail.com>
2017-10-18 12:43:30 -0400

    removing db dependency

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
 }
 
 // connect to the database
-var db = mongoose.connect(connectionString);
+var db = {};//mongoose.connect(connectionString);
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 5000;
